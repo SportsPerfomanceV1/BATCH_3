9c6636eaceab512e16420b1fb99ad7f2dba61079
kanika0304 <112932559+kanika0304@users.noreply.github.com>
2024-10-23 12:11:08 +0530

    structure

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './component/dashboard/Dashboard';
+import News from './component/news/News';
+import Event from './component/event/Event';
+import Coaches from './component/coaches/Coaches';
+import Athelete from './component/athelete/Athelete';
+import Result from './component/result/Result';
+import Navbar from './component/navbar/Navbar'; 
+import Login from './component/login/Login';
+import Register from './component/register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar /> 
+        <div className="App-header"> 
+          <div className="App-content">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/event" element={<Event />} />
+              <Route path="/coaches" element={<Coaches />} />
+              <Route path="/athelete" element={<Athelete />} />
+              <Route path="/result" element={<Result />} />
+              <Route path="/login" element={<Login />} /> {/* Route for Login */}
+              <Route path="/register" element={<Register />} /> {/* Route for Sign Up */}
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ fronted/src/component/athelete/Athelete.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Athelete() {
+  return (
+    <div>Athelete</div>
+  )
+}
+
+export default Athelete
