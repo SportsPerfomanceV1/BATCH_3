980baf5ef383c62ccdecb669c880cc8c01ac57c2
Kanika Sharma <112932559+Kanika0304@users.noreply.github.com>
2024-10-23 11:31:27 +0530

    Delete sports-ui directory

--- sports-ui/src/component/athelete/Athelete.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Athelete() {
-  return (
-    <div>Athelete</div>
-  )
-}
-
-export default Athelete
--- sports-ui/src/component/coaches/Coaches.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Coaches() {
-  return (
-    <div>Coaches</div>
-  )
-}
-
-export default Coaches
