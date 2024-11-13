e7cbbdadf3646bf79c9fb11342c2c4a222b5cd59
muhnihadkv <muhammednihad313@gmail.com>
2024-11-13 15:30:15 +0530

    initial commit for result publish

--- Backend/Event/src/main/java/com/SportsPerformance/batch3/Batch3Application.java
+++ Backend/Event/src/main/java/com/SportsPerformance/batch3/Batch3Application.java
@@ -2,8 +2,10 @@ package com.SportsPerformance.batch3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Batch3Application {
 
 	public static void main(String[] args) {
---
+++ Backend/Event/src/main/java/com/SportsPerformance/batch3/service/ResultService.java
@@ -0,0 +1,7 @@
+package com.SportsPerformance.batch3.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResultService {
+}
